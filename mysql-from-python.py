dddbd526b56e59b1046c45e6cf55558ddce17a79
lukehanson2429 <lukehanson2429@gmail.com>
2021-05-12 14:23:39 +0000

    SQL Learning

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 
@@ -15,10 +16,8 @@ connection = pymysql.connect(host='localhost',
 try: 
     #Run A query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        rows = cursor.executemany("DELETE FROM Friends WHERE name in ('jim, 'bob')")
+        connection.commit()
 finally:
     # Close the connection, regardless of whether the above was successful
     connection.close()
